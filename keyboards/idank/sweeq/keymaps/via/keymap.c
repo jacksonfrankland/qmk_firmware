c6d45eb37d5f85548c91f65f45e8c82e0c04422f
Jackson Frankland <contact@jacksonfrankland.com>
2023-11-05 13:42:21 +1100

    Add backspace to F layer

--- keyboards/idank/sweeq/keymaps/via/keymap.c
+++ keyboards/idank/sweeq/keymaps/via/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
   [3] = LAYOUT(
       KC_F1, KC_F2, KC_F3, KC_F4, KC_MCTL,       KC_LPAD, KC_MPRV, KC_MNXT, KC_MPLY, KC_NO,
-      KC_F5, KC_F6, KC_F7, KC_F8, KC_NO,         KC_NO, KC_VOLD, KC_VOLU, KC_MUTE, KC_NO,
+      KC_F5, KC_BSPC, KC_F7, KC_F8, KC_NO,         KC_NO, KC_VOLD, KC_VOLU, KC_MUTE, KC_NO,
       KC_F9, KC_F10, KC_F11, KC_F12, QK_BOOT,    KC_NO, KC_BRID, KC_BRIU, KC_NO, KC_NO,
                               KC_TRNS, KC_NO,    KC_NO, KC_TRNS
     )
